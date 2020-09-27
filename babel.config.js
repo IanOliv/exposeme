88bb5509e973654d352c3643bc04675a1096c227
Ian Oliveira <ian.oliv@outlook.com>
2020-09-27 00:37:12 -0300

    changin the babel config

--- babel.config.js
+++ babel.config.js
@@ -12,12 +12,20 @@ module.exports = {
   ],
   plugins: [
     ['module-resolver', {
-      alias: {
-        '@config': './src/config',
-        '@models': './src/models',
-        '@controllers': './src/controllers',
-        '@views': './src/views'
-      }
+      alias:  {
+      "@app/*": ["./src/app/*","./src/app/index.ts"],
+      "@server/*": ["./src/server/*"],
+      "@decorators/*": ["./src/decorators/*"],
+      "@client/*": ["./src/client/*"],
+      "@client": ["./src/client/index.ts"],
+      "@store": [
+        "./src/store/index.ts"],
+      "@store/*": [
+            "./src/store/*"
+        ],
+      "@types/*": ["./src/@types/*","./node_modules/@types/*"],
+      "@types": ["./src/@types/index.d.ts"]
+    }
     }]
   ],
   ignore: [
