ece0b8e07ba8fdd2e01eaabce922707ec1f57b2b
Ian Oliveira <ian.oliv@outlook.com>
2020-09-27 00:43:48 -0300

    removin yarn lock  & addin 2 gitignore

--- src/@types/dispatcher.d.ts
+++ src/@types/dispatcher.d.ts
@@ -1,6 +1,6 @@
 export interface iDispatcher {
-    private id:number
-    private  callbacks:{[name:string|number]}
+    id:number
+    callbacks:{[name:string|number]}
     register(callback:Function):number
     unregister(id:number):void
     dispatch(payload:unknown):void
